8292f77585b425d9f853bbec846e29af4320b713
kavyaumesh <58164633+kavyaumesh@users.noreply.github.com>
2021-06-21 21:54:09 +0530

    Add files via upload

---
+++ server-edit.py
@@ -0,0 +1,34 @@
+import socket
+
+SERVER = socket.gethostbyname(socket.gethostname())
+PORT = 46673        # Port to listen on (non-privileged ports are > 1023)
+ADDR = (SERVER , PORT)
+ssFT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ssFT.bind(ADDR)
+ssFT.listen(1)
+
+while True:
+    (conn, address) = ssFT.accept()
+    text_file = 'receivejoin.txt'
+
+    # Receive, output and save file
+    with open(text_file, "wb") as fw:
+        print("Receiving..")
+        while True:
+            print('receiving')
+            data = conn.recv(1024)
+            if data == b'BEGIN':
+                continue
+            elif data == b'ENDED':
+                print('Breaking from file write')
+                break
+            else:
+                print('Received: ', data.decode('utf-8'))
+                fw.write(data)
+                print('Wrote to file', data.decode('utf-8'))
+        fw.close()
+        print("Received..")
+    break
+ssFT.close()
+
+
